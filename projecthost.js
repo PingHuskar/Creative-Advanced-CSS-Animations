f8074310417247a66006955b48d0d74088b45425
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-08-26 16:13:12 +0700

    add table of content to each section

--- projecthost.js
+++ projecthost.js
@@ -24,15 +24,22 @@ const buttonsEffects = [
 	"creative wavy button effect",
 	"diagonal swipe button effect using borders"
 ]
-
+target = `_blank`
+var sectionTitle = `creative buttons effects using CSS transforms`
+var idea = `buttonsEffects`
+document.getElementById(`${idea}`).innerHTML += `<section><h3>Table of Content</h3><ul id="${idea}TOC"></ul></section>`
 for (var i = 0; i < buttonsEffects.length; i++) {
 	if (buttonsEffects[i] !== "") {
-		document.getElementById('buttonsEffects').innerHTML += `<section data-background-iframe="${hostpath}/creative buttons effects using CSS transforms/${buttonsEffects[i]}" data-background-interactive></section>`
+		document.getElementById(`${idea}`).innerHTML += `<section data-background-iframe="${hostpath}/${sectionTitle}/${buttonsEffects[i]}" data-background-interactive></section>`
+		document.getElementById(`${idea}TOC`).innerHTML += `<li><a href="${hostpath}/${sectionTitle}/${buttonsEffects[i]}" target="${target}">${buttonsEffects[i]}</a></li>`
 	}
 }
-
+sectionTitle = `creative images effects using CSS transforms`
+idea = `imagesEffects`
+document.getElementById(`${idea}`).innerHTML += `<section><h3>Table of Content</h3><ul id="${idea}TOC"></ul></section>`
 for (var i = 1; i < 12; i++) {
-	document.getElementById('imagesEffects').innerHTML += `<section data-background-iframe="${hostpath}/creative images effects using CSS transforms/creative image effect ${i}" data-background-interactive></section>`
+	document.getElementById(`${idea}`).innerHTML += `<section data-background-iframe="${hostpath}/${sectionTitle}/creative image effect ${i}" data-background-interactive></section>`
+    document.getElementById(`${idea}TOC`).innerHTML += `<li><a href="${hostpath}/${sectionTitle}/creative image effect ${i}" target="${target}">creative image effect ${i}</a></li>`
 }
 const menusEffects = [
     "2 borders and a background menu effect",
@@ -46,9 +53,13 @@ const menusEffects = [
     "creative menu effect using the transition delay property",
     "creative splitted background menu effect"
 ]
+sectionTitle = `creative menus effects using CSS transforms`
+idea = `menusEffects`
+document.getElementById(`${idea}`).innerHTML += `<section><h3>Table of Content</h3><ul id="${idea}TOC"></ul></section>`
 for (var i = 0; i < menusEffects.length; i++) {
 	if (menusEffects[i] !== "") {
-		document.getElementById('menusEffects').innerHTML += `<section data-background-iframe="${hostpath}/creative menus effects using CSS transforms/${menusEffects[i]}" data-background-interactive></section>`
+		document.getElementById(`${idea}`).innerHTML += `<section data-background-iframe="${hostpath}/${sectionTitle}/${menusEffects[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}TOC`).innerHTML += `<li><a href="${hostpath}/${sectionTitle}/${menusEffects[i]}" target="${target}">${menusEffects[i]}</a></li>`
 	}
 }
 const cardsEffects = [
@@ -60,9 +71,13 @@ const cardsEffects = [
     "creative layered card hover effect",
     "creative scaling card effect"
 ]
+sectionTitle = `creative CSS cards effects using CSS transforms`
+idea = `cardsEffects`
+document.getElementById(`${idea}`).innerHTML += `<section><h3>Table of Content</h3><ul id="${idea}TOC"></ul></section>`
 for (var i = 0; i < cardsEffects.length; i++) {
     if (cardsEffects[i] !== "") {
-        document.getElementById('cardsEffects').innerHTML += `<section data-background-iframe="${hostpath}/creative CSS cards effects using CSS transforms/${cardsEffects[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}`).innerHTML += `<section data-background-iframe="${hostpath}/${sectionTitle}/${cardsEffects[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}TOC`).innerHTML += `<li><a href="${hostpath}/${sectionTitle}/${cardsEffects[i]}" target="${target}">${cardsEffects[i]}</a></li>`
     }
 }
 
@@ -72,9 +87,13 @@ const transformEffects = [
     "smoky text effect using css transforms",
     "social media icons hover effect with css transforms"
 ]
+sectionTitle = `other cool examples with CSS transforms`
+idea = `transformEffects`
+document.getElementById(`${idea}`).innerHTML += `<section><h3>Table of Content</h3><ul id="${idea}TOC"></ul></section>`
 for (var i = 0; i < transformEffects.length; i++) {
     if (transformEffects[i] !== "") {
-        document.getElementById('transformEffects').innerHTML += `<section data-background-iframe="${hostpath}/other cool examples with CSS transforms/${transformEffects[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}`).innerHTML += `<section data-background-iframe="${hostpath}/${sectionTitle}/${transformEffects[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}TOC`).innerHTML += `<li><a href="${hostpath}/${sectionTitle}/${transformEffects[i]}" target="${target}">${transformEffects[i]}</a></li>`
     }
 }
 
@@ -111,9 +130,13 @@ const animations = [
     "text reveal effect with CSS animations",
     "text rotator using css animation"
 ]
+sectionTitle = `CSS animations creative examples`
+idea = `animations`
+document.getElementById(`${idea}`).innerHTML += `<section><h3>Table of Content</h3><ul id="${idea}TOC"></ul></section>`
 for (var i = 0; i < animations.length; i++) {
     if (animations[i] !== "") {
-        document.getElementById('animations').innerHTML += `<section data-background-iframe="${hostpath}/CSS animations creative examples/${animations[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}`).innerHTML += `<section data-background-iframe="${hostpath}/${sectionTitle}/${animations[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}TOC`).innerHTML += `<li><a href="${hostpath}/${sectionTitle}/${animations[i]}" target="${target}">${animations[i]}</a></li>`
     }
 }
 
@@ -128,9 +151,13 @@ const clipPath = [
     "creative split loading effect using CSS clip path property",
     "image to circle hover effect using CSS clip path property"
 ]
+sectionTitle = `CSS clip path property examples`
+idea = `clipPath`
+document.getElementById(`${idea}`).innerHTML += `<section><h3>Table of Content</h3><ul id="${idea}TOC"></ul></section>`
 for (var i = 0; i < clipPath.length; i++) {
     if (clipPath[i] !== "") {
-        document.getElementById('clipPath').innerHTML += `<section data-background-iframe="${hostpath}/CSS clip path property examples/${clipPath[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}`).innerHTML += `<section data-background-iframe="${hostpath}/${sectionTitle}/${clipPath[i]}" data-background-interactive></section>`
+        document.getElementById(`${idea}TOC`).innerHTML += `<li><a href="${hostpath}/${sectionTitle}/${clipPath[i]}" target="${target}">${clipPath[i]}</a></li>`
     }
 }
 
